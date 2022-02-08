69c5ff7bd02617b806e575132ba01e0664acbcf2
NexusProgrammingStudio <60765914+NexusProgrammingStudio@users.noreply.github.com>
2022-02-08 13:35:19 +0530

    Initial commit

---
+++ Assets/Scripts/BasicMover.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        
+    }*/
+    public float spinSpeed = 180.0f;
+    public float motionMagnitude = 0.1f;
+
+    public bool doSpin = true;
+    public bool doMotion = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(doSpin)
+        {
+            // Rotate around the up/x axis of thr gameObject
+            gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+        }
+        
+        if(doMotion)
+        {
+            // Move Up/Down over time
+            gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+        }
+    }
+}
